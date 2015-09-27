e9ac03ed84feb24a716d21b1e6a61b3f9445f026
Peska <peska_jr@o2.pl>
2015-09-27 21:17:14 +0200

    Playing with layouts

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+	public class HomeController : Controller
+	{
+		Product product = new Product()
+		{
+			ProductID = 1,
+			Name = "Kayak",
+			Description = "A boat for one person",
+			Category = "Watersports",
+			Price = 275M
+		};
+
+		public ActionResult Index()
+		{
+			return View(product);
+		}
+
+		public ActionResult NameAndPrice()
+		{
+			return View(product);
+		}
+	}
+}
